e921b2ad3d0ebf09e8fd7145842183b5ba650237
Didi <danielb@edintattoo.co.uk>
2024-08-24 12:54:28 +0100

    Fixed additional data fields for stories

--- lib/asanaficator/story.ex
+++ lib/asanaficator/story.ex
@@ -19,6 +19,7 @@ defmodule Asanaficator.Story do
     created_by: nil, # Asanaficator.User
     resource_subtype: "",
     text: "",
+    target: nil, #Asanaficator.Task (SUBJECT TO CHANGE IN API!!)
     data: nil
   )
 
@@ -29,11 +30,13 @@ defmodule Asanaficator.Story do
     created_by: Asanaficator.User,
     resource_subtype: binary,
     text: binary,
+    target: Asanaficator.Task,
     data: Map
     }
 
   @nest_fields %{
-    created_by: Asanaficator.User
+    created_by: Asanaficator.User, 
+    target: Asanaficator.Task
     } 
 
   def get_nest_fields(), do: @nest_fields 
@@ -41,19 +44,48 @@ defmodule Asanaficator.Story do
   @spec new() :: t
   def new(), do: struct(Asanaficator.Story)
 
+  defp store_optionals(data) do
+    story_keys = %Asanaficator.Story{}
+                  |> Map.keys()
+                  |> Enum.map(&Atom.to_string/1)
+    opt_data =
+      data
+      |> Enum.filter(fn {k, _v} -> 
+        not Enum.member?(story_keys, k) 
+      end)
+      |> Enum.into(%{}) 
+    IO.inspect(opt_data)
+
+  end
   @doc """ 
   Get a single story.
   ## Example 
-    Asanaficator.Story.get_story(client, 1337 :: story_id, {optfields: likes}
+    Asanaficator.Story.get_story(client, 1337 :: story_id, %{optfields: likes}
   
   NOTE: If no opt fields are given, relevant conditional fields will be returned and placed in the data field of your story.
 
   More info at: https://developers.asana.com/reference/getstory
   """
-#  @spec get_story(Client.t, integer | binary, List.t) :: Asanaficator.Story.t
+  @spec get_story(Client.t, integer | binary, List.t) :: Asanaficator.Story.t
   def get_story(client \\ %Client{}, story_id, params \\ []) do
     response = get(client, "stories/#{story_id}", params)
-    IO.inspect(response)
+    opt_data = store_optionals(response["data"])
+    story = cast(Asanaficator.Story, response["data"], @nest_fields)
+    Map.put(story, "data", opt_data)
+  end
+
+  @doc """
+  Get stories associated with a task.
+  ## Example
+    Asanaficator.Story.get_task_stories(client, 1337 :: task_id, %{optfields: likes}
+
+    More info at: https://developers.asana.com/reference/getstoriesfortask
+  """
+  @spec get_task_stories(Client.t, integer | binary, List.t) :: [Asanaficator.Story.t]
+  def get_task_stories(client \\ %Client{}, task_id, params \\ []) do
+    response = get(client, "tasks/#{task_id}/stories", params)
     cast(Asanaficator.Story, response["data"], @nest_fields)
   end
 end
+
+
